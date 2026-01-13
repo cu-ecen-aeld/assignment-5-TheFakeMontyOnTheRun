f1687a503ba27c2126751e5733d0f1198db1ef3a
Daniel Monteiro <monteiroquiet@gmail.com>
2026-01-13 23:53:50 +0000

    configure repo for assignment 3
    The hash will have to eventually change, but for now, it was supposed to work.
    Things build to a certain point, but doesn't fully work yet.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,12 +5,11 @@
 #
 ##############################################################
 
-#TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '#COMMIT VERSION NUMBER'
-# Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
+
+AESD_ASSIGNMENTS_VERSION = 68649926d111295afc5dfb09f9dac09a9695f959
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = '#GITHUB REPOSITORY LINK'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-TheFakeMontyOnTheRun.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
@@ -22,6 +21,9 @@ endef
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/finder.sh $(TARGET_DIR)/etc/finder-app
+	$(INSTALL) -m 0755 $(@D)/finder-test.sh $(TARGET_DIR)/etc/finder-app
+	$(INSTALL) -m 0755 $(@D)/writer $(TARGET_DIR)/etc/finder-app
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
 endef
 
